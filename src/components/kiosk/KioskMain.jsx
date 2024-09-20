7799250a551927d67af2d96fdb52635a5cfaf55b
zerock <zk2020308a@gmail.com>
2024-09-20 10:30:58 +0900

    1-1

--- src/components/kiosk/KioskMain.jsx
+++ src/components/kiosk/KioskMain.jsx
@@ -2,10 +2,20 @@ import ProductsList from "./ProductsList.jsx";
 import CartDiv from "./CartDiv.jsx";
 
 function KioskMain() {
+
+    const products = [
+        {pid:1, pname:'M1', price:8000, kind: 'N', img:'http://localhost:8081/food/M1.jpeg'},
+        {pid:2, pname:'M2', price:6000, kind: 'C', img:'http://localhost:8081/food/M2.jpeg'},
+        {pid:3, pname:'M3', price:7000, kind: 'N', img:'http://localhost:8081/food/M3.jpeg'},
+        {pid:4, pname:'M4', price:5000, kind: 'C', img:'http://localhost:8081/food/M4.jpeg'},
+        {pid:5, pname:'M5', price:9000, kind: 'N', img:'http://localhost:8081/food/M5.jpeg'},
+    ]
+
+
     return (
         <div className='w-full flex'>
             <div className='border-2 w-2/3'>
-                <ProductsList></ProductsList>
+                <ProductsList products={products}></ProductsList>
             </div>
             <div className='border-2 w-1/3'>
                 <CartDiv></CartDiv>
--- src/components/kiosk/ProductsList.jsx
+++ src/components/kiosk/ProductsList.jsx
@@ -1,8 +1,21 @@
 
-function ProductsList() {
+// eslint-disable-next-line react/prop-types
+function ProductsList({products}) {
+
+    const liList = products.map( p => {
+        const {pid, pname, price, kind, img} = p
+
+        return <li className='w-1/6 border-2 h-1/6 min-h-[10rem]' key={pid}>
+            <img src={img}/>
+            {pname} -- {price}
+        </li>
+    })
+
     return (
         <div className='bg-amber-500 w-full h-full'>
-
+        <ul className='flex gap-2 flex-auto p-6 ' >
+            {liList}
+        </ul>
         </div>
     );
 }
